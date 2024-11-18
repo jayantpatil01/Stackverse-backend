ea665c147db336f2ff3a8cc2cf1e6814761bbefc
Jayant Patil <patiljayant2023@gmail.com>
2024-11-18 21:32:04 +0530

    removed app link

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const paymentRoutes = require("./routes/paymentRoutes");
+const path = require("path");
+
 
 // Load environment variables
 dotenv.config();
@@ -9,6 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/payment", paymentRoutes);
 
